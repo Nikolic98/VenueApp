f2c6f30f663b5042dd02d53473e5e9ab46849a62
Marko Nikolic <markonikolic770@gmail.com>
2023-04-09 10:27:10 +0200

    Add necessary libraries

--- app/src/main/java/com/example/venueapp/MainActivity.kt
+++ app/src/main/java/com/example/venueapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.venueapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.venueapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun crashTest() {
+//        val crashButton = Button(this)
+//        crashButton.text = "Test Crash"
+//        crashButton.setOnClickListener {
+//            throw RuntimeException("Test Crash") // Force a crash
+//        }
+//
+//        addContentView(crashButton, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
